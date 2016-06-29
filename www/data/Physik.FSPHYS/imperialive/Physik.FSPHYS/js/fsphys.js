9eb732040f94861006f405d9afa2a474fbf0b300
Simon May <simon.may@uni-muenster.de>
2016-06-30 00:59:18 +0200

    Include CSS and JS files

---
+++ www/data/Physik.FSPHYS/imperialive/Physik.FSPHYS/js/fsphys.js
@@ -0,0 +1,31 @@
+// remove "index.html" or "index.php" as well as file endings in all internal
+// URLs (since they are redirected on the server side to the "clean" versions)
+// anyway
+function clean_links(list) {
+	var re = /^(https?:[/][/](www|sso)\.(uni-muenster|wwu)\.de)?[/]Physik.FSPHYS[/](.*?)(index)?\.(html|php)$/;
+	for (var i = 0; i < list.length; i++) {
+		var l = list[i];
+		if (l.tagName.toLowerCase() != 'a') {
+			continue;
+		}
+		var new_href = l.href.replace(re, '/Physik.FSPHYS/$4');
+		if (l.href != new_href) {
+			l.href = new_href;
+		}
+	}
+}
+document.addEventListener('DOMContentLoaded', function() {
+	var all_links = document.getElementsByTagName('a');
+	clean_links(all_links);
+	// since the navigation is dynamically loaded, check for DOM changes and clean
+	// any new URLs too
+	var observer = new MutationObserver(function(mutations) {
+		mutations.forEach(function(mutation) {
+			var new_links = mutation.target.getElementsByTagName('a');
+			clean_links(new_links);
+		});    
+	});
+	observer.observe(document.body, {childList: true, subtree: true});
+});
+
+
