c27467bb8b55a5e326e73b1f46ea86aa49edf7d2
Simon May <simon.may@uni-muenster.de>
2016-06-30 02:39:37 +0200

    Small change to fsphys.js
    Move function definition inside the anonymous function so that the global
    namespace is not affected.

--- www/data/Physik.FSPHYS/imperialive/Physik.FSPHYS/js/fsphys.js
+++ www/data/Physik.FSPHYS/imperialive/Physik.FSPHYS/js/fsphys.js
@@ -1,20 +1,20 @@
-// remove "index.html" or "index.php" as well as file endings in all internal
-// URLs (since they are redirected on the server side to the "clean" versions)
-// anyway
-function clean_links(list) {
-	var re = /^(https?:[/][/](www|sso)\.(uni-muenster|wwu)\.de)?[/]Physik.FSPHYS[/](.*?)(index)?\.(html|php)$/;
-	for (var i = 0; i < list.length; i++) {
-		var l = list[i];
-		if (l.tagName.toLowerCase() != 'a') {
-			continue;
-		}
-		var new_href = l.href.replace(re, '/Physik.FSPHYS/$4');
-		if (l.href != new_href) {
-			l.href = new_href;
+document.addEventListener('DOMContentLoaded', function() {
+	// remove "index.html" or "index.php" as well as file endings in all internal
+	// URLs (since they are redirected on the server side to the "clean" versions)
+	// anyway
+	function clean_links(list) {
+		var re = /^(https?:[/][/](www|sso)\.(uni-muenster|wwu)\.de)?[/]Physik.FSPHYS[/](.*?)(index)?\.(html|php)$/;
+		for (var i = 0; i < list.length; i++) {
+			var l = list[i];
+			if (l.tagName.toLowerCase() != 'a') {
+				continue;
+			}
+			var new_href = l.href.replace(re, '/Physik.FSPHYS/$4');
+			if (l.href != new_href) {
+				l.href = new_href;
+			}
 		}
 	}
-}
-document.addEventListener('DOMContentLoaded', function() {
 	var all_links = document.getElementsByTagName('a');
 	clean_links(all_links);
 	// since the navigation is dynamically loaded, check for DOM changes and clean
