5ee7d3312431633553a68f296431a769e723ce38
Simon May <simon.may@uni-muenster.de>
2016-08-12 05:53:35 +0200

    Rewrite schedule editor with the new design

--- www/data/Physik.FSPHYS/imperialive/Physik.FSPHYS/intern/intern-fs/praesenzplan.php
+++ www/data/Physik.FSPHYS/imperialive/Physik.FSPHYS/intern/intern-fs/praesenzplan.php
@@ -1,278 +1,200 @@
-<article class="module extended"><div class="module-content">
-
 <?php
-require_once __DIR__ . '/admin/php-include/office_hours.inc';
-	
-function praesenzplan_anzeigen() {
-	$db = mysql_db_connect();
-	$sql = 'SELECT * FROM praesenzplan ORDER BY zeit ASC;';
-	$query = $db->query($sql);
-	echo <<<HTML
-	<script type="text/javascript">
-		function redirect(target) {
-			window.location.href = target;
-		}
-	</script>
-	<table style="width: 100%;">
-		<colgroup>
-			<col style="width: 10%;">
-			<col style="width: 18%;">
-			<col style="width: 18%;">
-			<col style="width: 18%;">
-			<col style="width: 18%;">
-			<col style="width: 18%;">
-		</colgroup>
-		<tr>
-			<th>Zeit</th>
-			<th>Montag</th>
-			<th>Dienstag</th>
-			<th>Mittwoch</th>
-			<th>Donnerstag</th>
-			<th>Freitag</th>
-		</tr>
-HTML;
-	while ($row = $query->fetch()) {
-		$time = $row['zeit'];
-		$javascript = function($day, $time) {
-			return <<<JS
-			onmouseover="this.style='background-color: #d9ee83;';" onmouseout="this.style='';" onmousedown="redirect('?praesenzplan=true&amp;day=$day&amp;time=$time');"
-JS;
-		};
-		echo <<<HTML
-		<tr>
-			<td>$time</td>
-			<td {$javascript('montag', $time)}>{$row['montag']}</td>
-			<td {$javascript('dienstag', $time)}>{$row['dienstag']}</td>
-			<td {$javascript('mittwoch', $time)}>{$row['mittwoch']}</td>
-			<td {$javascript('donnerstag', $time)}>{$row['donnerstag']}</td>
-			<td {$javascript('freitag', $time)}>{$row['freitag']}</td>
-		</tr>
-HTML;
-	}
-	echo '</table>';
-	mysql_db_close($db);
+use de\uni_muenster\fsphys;
+require_once 'office_hours.inc';
+
+function dget($dict, $key, $default=NULL) {
+	return isset($dict[$key]) ? $dict[$key] : $default;
 }
 
-function fepraesenzplan_anzeigen() {
-	$db = mysql_db_connect();
-	$sql = 'SELECT * FROM fepraesenzplan ORDER BY ID ASC;';
-	$query = $db->query($sql);
-	echo <<<HTML
-	<script type="text/javascript">
-		function redirect(target) {
-			window.location.href = target;
+function keys_set($arr, ...$keys) {
+	foreach ($keys as $key) {
+		if (!isset($arr[$key])) {
+			return false;
 		}
-	</script>
-
-	<table style="width: 100%;">
-	<colgroup>
-		<col style="width: 10%" />
-		<col style="width: 20%" />
-		<col style="width: 20%" />
-		<col style="width: 10%" />
-		<col style="width: 10%" />
-		<col style="width: 30%" />
-	</colgroup>
-	<tr>
-		<th>Nr.</th>
-		<th>Wochentag</th>
-		<th>Datum</th>
-		<th>Startzeit</th>
-		<th>Endzeit</th>
-		<th>Name</th>
-	</tr>
-HTML;
-	$javascript = function($col, $id) {
-		return <<<JS
-		onmouseover="this.style='background-color: #d9ee83;';" onmouseout="this.style='';" onmousedown="redirect('?fepraesenzplan=true&amp;col=$col&amp;id=$id');"
-JS;
-	};
-	while ($row = $query->fetch()) {
-		$id = $row['ID'];
-		$nr = $id + 1;
-		echo <<<HTML
-		<tr>
-			<td>$nr</td>
-			<td {$javascript('Wochentag', $id)}>{$row['Wochentag']}</td>
-			<td {$javascript('Datum', $id)}>{$row['Datum']}</td>
-			<td {$javascript('starttime', $id)}>{$row['starttime']}</td>
-			<td {$javascript('endtime', $id)}>{$row['endtime']}</td>
-			<td {$javascript('name', $id)}>{$row['name']}</td>
-		</tr>
-HTML;
 	}
-	echo '</table>';
-	mysql_db_close($db);
+	return true;
 }
 
-function aendere_pzeit($day, $time, $person) {
-	if (!ctype_alnum($day) || $day == 'zeit') {
+function update_times($db, $time_col, $old_val, $new_val) {
+	if (!in_array($time_col, ['start_time', 'end_time'])) {
 		return false;
 	}
-	$db = mysql_db_connect();
-	$sql = "UPDATE praesenzplan SET $day = :person WHERE zeit = :time;";
+	$sql = <<<SQL
+	UPDATE office_hours SET $time_col = :new_val WHERE $time_col = :old_val;
+SQL;
 	$query = $db->prepare($sql);
-	$query->bindValue(':person', $person);
-	$query->bindValue(':time', $time);
+	$query->bindValue(':old_val', $old_val);
+	$query->bindValue(':new_val', $new_val);
 	$result = $query->execute();
-	mysql_db_close($db);
 	return $result;
 }
 
-function maske_aendere_pzeit($day, $time) {
-	$db = mysql_db_connect();
-	$sql = 'SELECT * FROM praesenzplan WHERE zeit = :time;';
-	$query = $db->prepare($sql);	
-	$query->bindValue(':time', $time);
-	$query->execute();
-	$row = $query->fetch();
-	mysql_db_close($db);
-	
-	$person = $row[$day];
-	echo <<<HTML
-	<form method="POST" action="?praesenzplan=true">
-		<input type="hidden" name="day" value="$day">
-		<input type="hidden" name="time" value="$time">
-		<div class="center">
-			<div style="text-align: center;">
-				<input type="text" name="pzeit" size="50" value="$person" />
-			</div>
-			<div style="text-align: center; margin-bottom: 6ex;">
-				<input type="submit" value="Eintragen" />
-			</div>
-		</div>
-	</form>
-HTML;
-}
-
-function aendere_fepzeit($col, $id, $content) {
-	if (!ctype_alnum($col) || $col == 'ID') {
-		return false;
+function save_shows($db) {
+	$data = $_POST['r'];
+	$PK_CONDITION = fsphys\BREAK_PK_CONDITION;
+	$sql = <<<SQL
+	UPDATE office_hours_break SET "show" = :show WHERE $PK_CONDITION;
+SQL;
+	$query = $db->prepare($sql);
+	foreach ($data as $row) {
+		if (!is_array($row)) {
+			continue;
+		}
+		$query->bindValue(':day', dget($row, 'date'));
+		$query->bindValue(':start_time', dget($row, 'start_time'));
+		$query->bindValue(':end_time', dget($row, 'end_time'));
+		$query->bindValue(':show', isset($row['show']), \PDO::PARAM_BOOL);
+		$query->execute();
 	}
-	$db = mysql_db_connect();
-	$sql = "UPDATE fepraesenzplan SET $col = :content WHERE ID = :id;";
-	$query = $db->prepare($sql);
-	$query->bindValue(':content', $content);
-	$query->bindValue(':id', $id);
-	$result = $query->execute();
-	mysql_db_close($db);
-	return $result;
-}
-
-function maske_aendere_fepzeit($col, $id) {
-	$db = mysql_db_connect();
-	$sql = 'SELECT * FROM fepraesenzplan WHERE ID = :id;';
-	$query = $db->prepare($sql);
-	$query->bindValue(':id', $id);
-	$query->execute();
-	$result = $query->fetch();
-	mysql_db_close($db);
-	$content = $result[$col];
-
-	echo <<<HTML
-	<form method="POST" action="?fepraesenzplan=true">
-		<input type="hidden" name="col" value="$col" />
-		<input type="hidden" name="id" value="$id" />
-		<div class="center">
-			<div style="text-align: center;">
-				<input type="text" name="pzeit" size="50" value="$content" />
-			</div>
-			<div style="text-align: center; margin-bottom: 6ex;">
-				<input type="submit" value="Eintragen" />
-			</div>
-		</div>
-	</form>
-HTML;
-}
-
-function aendere_anzahl($count) {
-	$db = mysql_db_connect();
-	$sql = 'UPDATE fepraesenzplan SET Anzahl = :count;';
-	$query = $db->prepare($sql);
-	$query->bindValue(':count', $count);
-	$result = $query->execute();
-	mysql_db_close($db);
-	return $result;
-}
-
-function maske_aendere_anzahl() {
-	$db = mysql_db_connect();
-	$sql = 'SELECT Anzahl FROM fepraesenzplan WHERE ID = 0;';
-	$query = $db->query($sql);
-	$row = $query->fetch();
-	mysql_db_close($db);
-	
-	$count = $row[0];
-	echo <<<HTML
-	<form method="POST" action="?fepraesenzplan=true">
-		<div class="center">
-			<div style="text-align: center;">
-				Anzahl der anzuzeigenden Termine:
-			</div>
-			<div style="text-align: center;">
-				<input type="text" name="Anzahl" size="10" value="$count" />
-			</div>
-			<div style="text-align: center; margin-bottom: 6ex;">
-				<input type="submit" value="Eintragen" />
-			</div>
-		</div>
-	</form>
-HTML;
 }
 ?>
 
-<p style="text-align: center;">
-<a href="/Physik.FSPHYS/intern/intern-fs/praesenzplan?praesenzplan=true">Präsenzplan</a> |
-<a href="/Physik.FSPHYS/intern/intern-fs/praesenzplan?fepraesenzplan=true">Ferienpräsenzplan</a></p>
+<article class="module extended">
+<div class="module-content">
+	<p style="text-align: center;">
+	<a href="?break="><?=fsphys\loc_get_str('office hours', true);?></a> |
+	<a href="?break=1"><?=fsphys\loc_get_str('office hours (break)', true);?></a>
+	</p>
 
 <?php
-if ($_GET['praesenzplan']) {
-	$show_mask = isset($_GET['day']) && isset($_GET['time']);
-	$change_entry = isset($_POST['day']) && isset($_POST['time']) && isset($_POST['pzeit']);
-	if ($show_mask) {
-		$day = $_GET['day'];
-		$time = $_GET['time'];
-		$person = maske_aendere_pzeit($day, $time);
+if (isset($_GET['break'])) {
+	$break = boolval($_GET['break']);
+	// determine which state of the page we’re in
+	// if $show_mask_entry is true, $show_mask_time is also true
+	$show_mask_entry = keys_set($_GET, 'day', 'start_time', 'end_time', 'col');
+	$show_mask_time = keys_set($_GET, 'start_time', 'end_time', 'col');
+	$show_mask_new_entry = keys_set($_GET, 'new_entry');
+	$delete_entry = keys_set($_GET, 'day', 'start_time', 'end_time', 'delete');
+	$save_shows = $break && keys_set($_POST, 'save_shows', 'r');
+	$change_entry = keys_set($_POST, 'day', 'start_time', 'end_time', 'col',
+		'val');
+	$new_entry = keys_set($_POST, 'day', 'start_time', 'end_time',
+		'new_entry');
+
+	$data_arr = $show_mask_time || $delete_entry ? $_GET : $_POST;
+	$data_arr = array_map(function($x) {
+		return is_string($x) ? htmlspecialchars($x) : NULL;
+	}, $data_arr);
+	extract($data_arr, EXTR_PREFIX_ALL, 'req');
+	$db = fsphys\mysql_db_connect();
+	if ($show_mask_time) {
+		if ($show_mask_entry) {
+			$req_day = $break ? $req_day : intval($req_day);
+			$val = fsphys\office_hours_get($db, $break, $req_day,
+				$req_start_time, $req_end_time, $req_col);
+		}
+		// only $show_mask_time
+		else {
+			$req_day = '';
+			$val = $data_arr[$req_col];
+		}
+		$val = htmlspecialchars($val);
+?>
+		<form method="post" action="?break=<?=$break?>">
+			<input type="hidden" name="day" value="<?=$req_day?>" />
+			<input type="hidden" name="start_time"
+				value="<?=$req_start_time?>" />
+			<input type="hidden" name="end_time" value="<?=$req_end_time?>" />
+			<input type="hidden" name="col" value="<?=$req_col?>" />
+			<div class="center">
+				<input type="text" name="val" size="50"
+					value="<?=$val?>" />
+				<div style="margin-top: 3ex;">
+					<input type="submit" value="Eintragen" />
+				</div>
+			</div>
+		</form>
+<?php
 	}
-	else if ($change_entry) {
-		$day = $_POST['day'];
-		$time = $_POST['time'];
-		$person = $_POST['pzeit'];
-		aendere_pzeit($day, $time, $person);
-		praesenzplan_anzeigen();
+	elseif ($show_mask_new_entry) {
+?>
+		<form method="post" action="?break=<?=$break?>">
+			<label>
+				<?=fsphys\loc_get_str('date', true)?>:
+				<input type="date" name="day" required />
+			</label>
+			<label>
+				<?=fsphys\loc_get_str('start time', true)?>:
+				<input type="time" name="start_time" required />
+			</label>
+			<label>
+				<?=fsphys\loc_get_str('end time', true)?>:
+				<input type="time" name="end_time" required />
+			</label>
+			<label>
+				<?=fsphys\loc_get_str('name', true)?>:
+				<input type="text" name="name" />
+			</label>
+			<label>
+				<input type="checkbox" name="show" value="" />
+				<?=fsphys\loc_get_str('show', true)?>
+			</label>
+			<div class="center" style="margin-top: 3ex;">
+				<input type="submit" name="new_entry" value="Eintragen" />
+			</div>
+		</form>
+<?php
 	}
 	else {
-		praesenzplan_anzeigen();
+		if ($change_entry) {
+			// for database: use val exactly as POSTed
+			$raw_val = $_POST['val'];
+			if ($req_day) {
+				fsphys\office_hours_set($db, $break, $req_day, $req_start_time,
+					$req_end_time, $req_col, $raw_val);
+			}
+			// !$req_day means that times are being set in the non-break
+			// schedule
+			else {
+				update_times($db, $req_col, $data_arr[$req_col], $raw_val);
+			}
+		}
+		elseif ($new_entry) {
+			fsphys\office_hours_insert($db, true, [
+				'date' => $req_day,
+				'start_time' => $req_start_time,
+				'end_time' => $req_end_time,
+				// for database: use text exactly as POSTed
+				'name' => dget($_POST, 'name'),
+				'show' => isset($req_show)
+			]);
+		}
+		elseif ($save_shows) {
+			save_shows($db);
+		}
+		elseif ($delete_entry) {
+			fsphys\office_hours_delete($db, $break, $req_day, $req_start_time,
+				$req_end_time);
+		}
+		// show schedule
+		$options = fsphys\DEFAULT_HTML_OPTIONS;
+		$options['edit_mode'] = true;
+		if ($break) {
+			$break_schedule = fsphys\office_hours_break_html($db, $options);
+?>
+		<form method="post" action="?break=<?=$break?>">
+			<?=$break_schedule?>
+			<div class="center" style="margin-top: 3ex;">
+				<input type="submit" name="save_shows"
+					value="<?=fsphys\loc_get_str('save show setting', true)?>"
+				/>
+			</div>
+		</form>
+		<div class="center" style="margin-top: 3ex;">
+			<a href="?break=<?=$break?>&amp;new_entry">
+				<?=fsphys\loc_get_str('new entry', true)?>
+			</a>
+		</div>
+<?php
+		}
+		else {
+			echo fsphys\office_hours_html($db, $options);
+		}
 	}
-}
-else if ($_GET['fepraesenzplan']) {
-	$show_mask = isset($_GET['col']) && isset($_GET['id']);
-	$change_entry = isset($_POST['col']) && isset($_POST['id']) && isset($_POST['pzeit']);
-	$change_count = isset($_POST['Anzahl']);
-	if ($show_mask) {
-		$col = $_GET['col'];
-		$id = $_GET['id'];
-		maske_aendere_fepzeit($col, $id);
-	}
-	else if ($change_entry) {
-		$col = $_POST['col'];
-		$id = $_POST['id'];
-		$content = $_POST['pzeit'];
-		aendere_fepzeit($col, $id, $content);
-		fepraesenzplan_anzeigen();
-		maske_aendere_anzahl();
-	}
-	else if ($change_count) {
-		$count = $_POST['Anzahl'];
-		aendere_anzahl($count);
-		fepraesenzplan_anzeigen();
-		maske_aendere_anzahl();
-	}
-	else {
-		fepraesenzplan_anzeigen();
-		maske_aendere_anzahl();
-	}
+	fsphys\mysql_db_close($db);
 }
 ?>
-</div></article>
+
+</div>
+</article>
 
