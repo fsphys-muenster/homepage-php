a66034d4cc2432c6d754e6414ea70c3687143031
Simon May <simon.may@uni-muenster.de>
2017-02-10 11:32:56 +0100

    Move semester_info into separate utility class

--- www/data/Physik.FSPHYS/imperialive/Physik.FSPHYS/ssi/office_hours_today.php
+++ www/data/Physik.FSPHYS/imperialive/Physik.FSPHYS/ssi/office_hours_today.php
@@ -1,9 +1,7 @@
 <?php
 use de\uni_muenster\fsphys;
-use function de\uni_muenster\fsphys\loc_get_str;
+use de\uni_muenster\fsphys\{Localization as Loc, SemesterInfo, Settings};
 require_once 'init.inc';
-require_once 'settings.inc';
-require_once 'localization.inc';
 require_once 'office_hours.inc';
 
 // max. rows to show in preview for office hours during break
@@ -16,25 +14,25 @@ const OH_BREAK_MAX_ROWS = 10;
 <?php
 fsphys\run_and_catch(function() {
 ?>
-<h2><?=loc_get_str('current office hours', true)?></h2>
+<h2><?=Loc::get('current office hours', true)?></h2>
 
 <div class=subhead>
 <?php
 $dt_now = new \DateTime();
-$semester = fsphys\semester_info($dt_now)['during_semester'];
-$holidays = fsphys\get_setting('office_hours.start_page_holiday_message',
+$semester = SemesterInfo::get($dt_now)['during_semester'];
+$holidays = Settings::get('office_hours.start_page_holiday_message',
 	'int');
 if ($holidays) {
 ?>
-	<?=loc_get_str('holidays', true)?></div>
-	<?=loc_get_str('HOLIDAY_MSG')?>
+	<?=Loc::get('holidays', true)?></div>
+	<?=Loc::get('HOLIDAY_MSG')?>
 <?php
 }
 elseif ($semester) {
 	// display office hours for the day five hours in the future (so that e.g.
 	// the schedule for Tuesday isn’t still displayed at 23:30 Tuesday night)
 ?>
-	<?=loc_get_str('during the semester', true)?></div>
+	<?=Loc::get('during the semester', true)?></div>
 	<div class=fsphys_oh_front_page>
 		<?=fsphys\office_hours_html([],
 			$dt_now->add(new \DateInterval('PT5H')))?>
@@ -43,7 +41,7 @@ elseif ($semester) {
 }
 else {
 ?>
-	<?=loc_get_str('semester break', true)?></div>
+	<?=Loc::get('semester break', true)?></div>
 	<div class=fsphys_oh_front_page>
 		<?=fsphys\office_hours_break_html(['short' => true],
 			OH_BREAK_MAX_ROWS)?>
@@ -53,8 +51,8 @@ else {
 ?>
 
 <p><a class=int
-	href="/Physik.FSPHYS/<?=fsphys\loc_url_lang_code()?>termine/"><?=
-		loc_get_str('complete office hours schedule', true)?></a></p>
+	href="/Physik.FSPHYS/<?=Loc::url_lang_code()?>termine/"><?=
+		Loc::get('complete office hours schedule', true)?></a></p>
 <?php
 }); // fsphys\run_and_catch() end
 ?>
