7d07c93d81db7452551a12795bead71e64ce9b9b
2xB <31772910+2xB@users.noreply.github.com>
2020-10-18 00:59:19 +0200

    Added CRSF for public sites
    Everything outside *.uni-muenster.de (e.g. wwu.de) is forwarded to *.uni-muenster.de
    => No need to check these

---
+++ www/data/Physik.FSPHYS/php_include/de/uni_muenster/fsphys/CRFpublic.php
@@ -0,0 +1,62 @@
+<?php
+namespace de\uni_muenster\fsphys;
+require_once 'init.php';
+	
+class CSRFpublicException extends \UnexpectedValueException { }
+
+class CSRFpublic {
+	const VALID_ORIGINS = [
+		'https://sso.uni-muenster.de',
+		'https://xsso.uni-muenster.de',
+		'https://www.uni-muenster.de'
+	];
+	
+	/**
+	 * Indicates if request HTTP headers are valid (same/expected origin)
+	 *
+	 * @link https://www.owasp.org/index.php/Cross-Site_Request_Forgery_(CSRF)_Prevention_Cheat_Sheet#Verifying_Same_Origin_with_Standard_Headers
+	 * @return bool validity of the request headers. true if both ORIGIN and
+	 *              REFERER are each valid or unset, false otherwise
+	 * @see CSRF::VALID_ORIGINS
+	 */
+	static function http_headers_valid(): bool {
+		$origin = $_SERVER['HTTP_ORIGIN'] ?? NULL;
+		$referrer = $_SERVER['HTTP_REFERER'] ?? NULL;
+		$origin_present = isset($origin);
+		$origin_valid = !$origin_present || self::starts_with_one_of(
+			$origin, self::VALID_ORIGINS);
+		$referrer_present = isset($referrer);
+		$referrer_valid = !$referrer_present || self::starts_with_one_of(
+			$referrer, self::VALID_ORIGINS);
+		// let validation pass if neither header is present, but log the
+		// occurrence
+		if (!$origin_present && !$referrer_present) {
+			mail_and_log(new CSRFException('Neither ORIGIN nor REFERER HTTP '
+				. 'headers were present.'));
+			return true;
+		}
+		return $origin_valid && $referrer_valid;
+	}
+	
+	static function check_http_headers(): void {
+		$origin = $_SERVER['HTTP_ORIGIN'] ?? '–';
+		$referrer = $_SERVER['HTTP_REFERER'] ?? '–';
+		if (!self::http_headers_valid()) {
+			throw new CSRFException('CSRF validation failed – possible CSRF '
+				. 'attack!' . "\n"
+				. "ORIGIN  = $origin\n"
+				. "REFERER = $referer"
+			);
+		}
+	}
+	
+	private static function starts_with_one_of(string $haystack,
+		array $needles): bool {
+		foreach ($needles as $needle) {
+			if (Util::starts_with($haystack, $needle)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
